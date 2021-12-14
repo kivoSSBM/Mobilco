3d6d48478c0cfa976779af47ce3d32c1ce440f76
Alex Forsberg <alexanderevaldforsberg@gmail.com>
2021-12-14 13:25:41 +0100

    work in progress
    Dialog popup added

---
+++ app/src/main/java/com/example/fitlane/WorkoutDialog.kt
@@ -0,0 +1,24 @@
+package com.example.fitlane
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.example.fitlane.databinding.WorkoutHomepageBinding
+
+class WorkoutDialog : DialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val rootView: View = inflater.inflate(R.layout.fragment_dialog, container, false)
+
+        return rootView
+    }
+
+
+}

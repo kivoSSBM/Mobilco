acab20965303baeaba43df88afec63fae347fc58
Alex Forsberg <alexanderevaldforsberg@gmail.com>
2021-12-15 19:34:51 +0100

    Flow
    Added buttons so we can do a flow

--- app/src/main/java/com/example/fitlane/WorkoutDialog.kt
+++ app/src/main/java/com/example/fitlane/WorkoutDialog.kt
@@ -1,24 +1,44 @@
 package com.example.fitlane
 
+import android.app.AlertDialog
+import android.app.Dialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.FragmentTransaction
+import androidx.navigation.fragment.findNavController
+import com.example.fitlane.databinding.FragmentDialogBinding
 import com.example.fitlane.databinding.WorkoutHomepageBinding
 
 class WorkoutDialog : DialogFragment() {
+
+    private var _binding: FragmentDialogBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        var rootView: View = inflater.inflate(R.layout.fragment_dialog, container)
+    ): View {
+        _binding = FragmentDialogBinding.inflate(inflater, container, false)
 
-        return rootView
+       // var rootView: View = inflater.inflate(R.layout.fragment_dialog, container, false)
+
+        return binding.root
+       // return rootView
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.quickstart.setOnClickListener {
+            findNavController().navigate(R.id.action_select_workout_one)
+
+        }
+
+    }
 
 }
--- app/src/main/java/com/example/fitlane/exeercise_categories.kt
+++ app/src/main/java/com/example/fitlane/exeercise_categories.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.fitlane.databinding.FragmentExeerciseCategoriesBinding
+import com.example.fitlane.databinding.FragmentWorkoutScheduledBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,6 +24,9 @@ class exeercise_categories : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    private var _binding: FragmentExeerciseCategoriesBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -33,8 +39,17 @@ class exeercise_categories : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        _binding = FragmentExeerciseCategoriesBinding.inflate(inflater, container, false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_exeercise_categories, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.categoryLegs.setOnClickListener {
+            findNavController().navigate(R.id.action_workout_legs)
+        }
     }
 
     companion object {
